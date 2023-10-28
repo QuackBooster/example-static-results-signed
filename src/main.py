6c0cbdffaf6b04ab06fedbab74d4ef9555855169
Felipe Ruiz <felipe.ruiz@adapa.mx>
2023-10-27 23:47:26 -0600

    Refactor script
    Signed-off-by: Felipe Ruiz <felipe.ruiz@adapa.mx>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Shellbang | https://realpython.com/python-shebang/
 
+import base64
 import json
 
 from cryptography.hazmat.primitives import hashes
@@ -9,37 +10,13 @@ from cryptography.hazmat.primitives import serialization
 
 # Files | CONSTANTS
 # Those are going to be use outside ?
-# no, so, called with '_'
 _FILE_INPUT = "control_output.json"
 _FILE_OUTPUT = "attestations.json"
 _FILE_KEY = "private-key.pem"
 
-# encryption
-_ENCRYPT_ALGO = "sha256"
-_ENCRYPT_padding = "PKCS1v15"
-
+# attestation
 _ATTESATION_DICKEYS_DATA = "payload"
 _ATTESATION_DICKEYS_SIGN = "signature"
-
-# attestation
-"""
-"type": "static-analysis",
-"result": {
-    "vulnerabilities": 0,
-    "warning": 2,
-    "code_smells": 4
-},
-"user": "developer-1"
-"""
-
-# attestation_list json keys
-# file output
-"""{
-"payload" : {},
-"signature": ""
-"
-}
-"""
 
 
 def load_key():
@@ -55,7 +32,6 @@ def load_key():
 # https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/#signing
 def sign_attesation(private_key, _attesation: dict) -> dict:
     _padding = padding.PKCS1v15()
-
     signature = private_key.sign(_attesation, _padding, hashes.SHA256())
 
     return signature
@@ -93,14 +69,14 @@ if __name__ == "__main__":
         user_encode_data = control_data.encode("utf-8")
         signature = sign_attesation(_private_key, user_encode_data)
 
+        result = base64.b64encode(signature).decode()
+
         __aux_attesation = dict()
 
         # control data pretty formated
         __aux_attesation[_ATTESATION_DICKEYS_DATA] = control_data
 
         __aux_attesation[_ATTESATION_DICKEYS_SIGN] = signature
-
-        print(__aux_attesation)
 
         attestations.append(__aux_attesation)
 
